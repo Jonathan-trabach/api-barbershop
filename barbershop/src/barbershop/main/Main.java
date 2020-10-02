5b06afe7dc5eda7e76bd92a03c673fb93202d28d
globalsysjonathan <63593866+globalsysjonathan@users.noreply.github.com>
2020-10-01 22:22:12 -0300

    Commit inicial

---
+++ barbershop/src/barbershop/main/Main.java
@@ -0,0 +1,14 @@
+package barbershop.main;
+
+import barbershop.layout.TelaLogin;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+            
+            TelaLogin tl = new TelaLogin();
+            tl.setVisible(true);
+        
+        }
+}
